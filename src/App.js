e223c51ab60d68adbf6bd2142db6d83d76da3023
Chris Paschall <ChrisP1108@gmail.com>
2021-07-19 09:24:56 -0700

    Nav-Links Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { BrowserRouter } from'react-router-dom';
 import Header from './components/Header/Header.js';
 import './App.css';
 
 const App = () => {
 
   return (
-    <div className="main-background">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="main-background">
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, 
     Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 import '../../App.css';
 import './Header.css'
 
@@ -13,25 +14,37 @@ const Header = () => {
     }
 
     return (
-        <Navbar className='bottom-border'>
+        <Navbar className='bottom-border' sticky='top' expand='md'>
             <div className="container">
                 <NavbarBrand href="/">
                     THE PLANETS
                 </NavbarBrand>
                 <NavbarToggler onClick={toggleNav} className="mr-2" />
                 <Collapse isOpen={isNavOpen} navbar>
-                    <Nav navbar className="ml-auto text-center">
+                    <Nav navbar className="mx-auto">
                         <NavItem>
-                            <NavLink activeClassName="navactive" className="nav-link navstyle" to="/home">HOME</NavLink>
+                            <NavLink activeClassName="navactive" className="nav-link navstyle mercury" to="/mercury">MERCURY</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/software">SOFTWARE</NavLink>
+                            <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/venus">VENUS</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/about">ABOUT</NavLink>
+                            <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/earth">EARTH</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/contactus">CONTACT US</NavLink>
+                            <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/mars">MARS</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/jupiter">JUPITER</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/saturn">SATURN</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/uranus">URANUS</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink activeClassName="navactive" className="nav-link navstyle ml-md-2 ml-xl-5" to="/neptune">NEPTUNE</NavLink>
                         </NavItem>
                     </Nav>
                 </Collapse>
