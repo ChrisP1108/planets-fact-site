9e9ea574a79381453e0255868a933d120074ae31
Chris Paschall <ChrisP1108@gmail.com>
2021-07-20 18:21:08 -0700

    CSS Planet Files Created

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,9 +1,28 @@
+import '../../App.css';
+import './Page.css';
+import './Mercury.css';
+import './Venus.css';
+import './Earth.css';
+import './Mars.css';
+import './Jupiter.css';
+import './Saturn.css';
+import './Uranus.css';
+import './Neptune.css';
+
 const Page = ({ content }) => {
 
-    console.log(content);
     return (
-        <div>
-            
+        <div className='container'>
+            <div className="row">
+                <div className="col">
+                    <div>
+
+                    </div>
+                </div>
+                <div className="col">
+                    Test
+                </div>
+            </div>
         </div>
     )
 }
