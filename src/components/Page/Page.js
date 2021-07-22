7f49f3fcc125c9b0670b1300c62e880b3be24868
Chris Paschall <ChrisP1108@gmail.com>
2021-07-21 17:43:59 -0700

    Mobile Responsive Initial Setup

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -12,27 +12,34 @@ import './Neptune.css';
 const Page = ({ content }) => {
 
     return (
-        <div className='container-margin'>
-            <div className="row">
-                <div className="col">
-                    <h2>OVERVIEW</h2>
-                    <h2>STRUCTURE</h2>
-                    <h2>SURFACE</h2>
-                </div>
-                <div className="col">
-                    <div className={`Planet-${content.name}`}></div>
+        <>
+            <div className="row no-margin">
+                <div className="col-12 no-padding bottom-border">
+                    <div className="row m-0 justify-content-around">
+                        <div className="col-3 p-0">
+                            <h2>OVERVIEW</h2>
+                        </div>
+                        <div className="col-3 p-0">
+                            <h2 className="d-block d-md-none">STRUCTURE</h2>
+                            <h2 className="d-none d-md-block">INTERNAL STRUCTURE</h2>
+                        </div>
+                        <div className="col-3 p-0">
+                            <h2 className="d-block d-md-none">SURFACE</h2>
+                            <h2 className="d-none d-md-block">SURFACE GEOLOGY</h2>
+                        </div>
+                    </div>
                 </div>
                 <div className="col-12">
+                    <div className={`Planet-${content.name} mx-auto my-auto`}></div>
+                </div>
+                <div className="col-12 text-center">
                     <h1>{content.name.toUpperCase()}</h1>
                     <p>{content.overview.content}</p>
-                </div>
-                <div className="col-12">
-                    <p>Test</p>
                 </div>
                 {/* <div className={`Internal-${content.name}`}></div>
                 <div className={`Geology-${content.name}`}></div> */}
             </div>
-        </div>
+        </>
     )
 }
 
